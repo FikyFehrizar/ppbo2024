e5c4de8fb174a3607780af1308c12a489e4ad67c
FikyFehrizar <fikyfehrizar@gmail.com>
2024-09-04 20:43:40 +0700

    Praktikum 2b - Menggunakan Fungsi Bawaa

--- pertama.php
+++ pertama.php
@@ -1,3 +1,17 @@
 <?php
-echo "Hello Word";
-?>
+date_default_timezone_set("Asia/Jakarta");
+$nama = readline('Masukkan nama Anda: ');
+$waktu = date('H');
+//echo "Halo {$nama} sekarang pukul {$waktu}\n";
+if ($waktu>=19) {
+    echo "Selamat malam {$nama} sekarang pukul {$waktu}";
+}
+elseif ($waktu>=15){
+    echo "Selamat sore {$nama} sekarang pukul {$waktu}";
+}
+elseif ($waktu>=11){
+    echo "Selamat siang {$nama} sekarang pukul {$waktu}";
+}
+elseif ($waktu>=5){
+    echo"Selamat pagi {$nama} sekarang pukul{$waktu}";
+}
