9cea91826aadcbfd2ef8ff902582ebf1b0506ada
FikyFehrizar <fikyfehrizar@gmail.com>
2024-09-05 14:59:03 +0700

    Praktikum 4C - Membuat Constructor

--- ketiga.php
+++ ketiga.php
@@ -4,6 +4,10 @@ class Lingkaran
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function luas() : float {
         return self::PHI * $this->jari_jari * $this->jari_jari;
     }
@@ -20,6 +24,11 @@ class Bola
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
+
     public function volume() : float {
         return (4/3) * self::PHI * pow($this->jari_jari,3);
     }
@@ -31,6 +40,12 @@ class Tabung
     const PHI = 3.14;
     public $jari_jari; 
     public $tinggi;
+
+    public function __construct($jari_jari,$tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
 
     public function volume() : float {
         return self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
@@ -44,32 +59,31 @@ class Kerucut
     public $jari_jari;
     public $tinggi;
 
+    public function __construct($jari_jari,$tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
     public function volume() : float {
         return (1/3) * self::PHI * pow($this->jari_jari,2) * $this->tinggi;
     }
 }
-$lingkaran = new Lingkaran();
-$lingkaran->jari_jari = 4;
+$lingkaran = new Lingkaran(4);
 $luas_lingkaran = $lingkaran->luas();
 $keliling_lingkaran = $lingkaran->keliling();
 echo "luas lingkaran : {$luas_lingkaran}\n";
 echo "keliling lingkaran : {$keliling_lingkaran}\n";
 
 
-$bola = new Bola();
-$bola->jari_jari=4;
+$bola = new Bola(4);
 $volume_bola=$bola->volume();
 echo "volume bola : {$volume_bola}\n";
 
-$tabung = new Tabung();
-$tabung -> jari_jari = 4;
-$tabung -> tinggi =10;
+$tabung = new Tabung(4,10);
 $volume_tabung=$tabung->volume();
 echo "volume tabung : {$volume_tabung}\n";
 
-$nasi_tumpeng = new Kerucut();
-$nasi_tumpeng->jari_jari = 4;
-$nasi_tumpeng->tinggi= 10; 
+$nasi_tumpeng = new Kerucut(4,10); 
 $volume_nasi_tumpeng =$nasi_tumpeng -> volume();
 echo "volume kerucut : {$volume_nasi_tumpeng}";
 
