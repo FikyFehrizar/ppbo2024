827b5dad9142ff7709d94dc3d53b7cfadb2cd6f4
FikyFehrizar <fikyfehrizar@gmail.com>
2024-09-05 14:46:09 +0700

    Praktikum 4B - Membuat Atribut

--- ketiga.php
+++ ketiga.php
@@ -4,13 +4,13 @@ class Lingkaran
     const PHI = 3.14;
     public $jari_jari;
 
-    public function luas($jari_jari) : float {
-        return self::PHI*$jari_jari*$jari_jari;
+    public function luas() : float {
+        return self::PHI * $this->jari_jari * $this->jari_jari;
     }
 
 
-    public function keliling($jari_jari) : float {
-        return 2*self::PHI*$jari_jari;
+    public function keliling() : float {
+        return 2 * self::PHI * $this->jari_jari;
     }
 }
 
@@ -20,8 +20,8 @@ class Bola
     const PHI = 3.14;
     public $jari_jari;
 
-    public function volume($jari_jari) : float {
-        return (4/3)*self::PHI*pow($jari_jari,3);
+    public function volume() : float {
+        return (4/3) * self::PHI * pow($this->jari_jari,3);
     }
 }
 
@@ -32,8 +32,8 @@ class Tabung
     public $jari_jari; 
     public $tinggi;
 
-    public function volume($jari_jari,$tinggi) : float {
-        return self::PHI*pow($jari_jari,2)*$tinggi;
+    public function volume() : float {
+        return self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
     }
 }
 
@@ -44,37 +44,33 @@ class Kerucut
     public $jari_jari;
     public $tinggi;
 
-    public function volume($jari_jari,$tinggi) : float {
-        return (1/3)*self::PHI*pow($jari_jari,2)*$tinggi;
+    public function volume() : float {
+        return (1/3) * self::PHI * pow($this->jari_jari,2) * $this->tinggi;
     }
 }
-$lingkaran= new Lingkaran();
-$lingkaran->$jari_jari(4);
-$lingkaran=new lingkaran(4);
-$luas_lingkaran=$lingkaran-> luas($jari_jari);
-$keliling_lingkaran=$lingkaran->keliling($jari_jari);
+$lingkaran = new Lingkaran();
+$lingkaran->jari_jari = 4;
+$luas_lingkaran = $lingkaran->luas();
+$keliling_lingkaran = $lingkaran->keliling();
 echo "luas lingkaran : {$luas_lingkaran}\n";
 echo "keliling lingkaran : {$keliling_lingkaran}\n";
 
 
 $bola = new Bola();
-$bola->$jari_jari=4;
-$bola = new Bola(4);
-$volume_bola=$bola->volume($jari_jari);
-echo "volume bola : {$volume_bola}/n";
+$bola->jari_jari=4;
+$volume_bola=$bola->volume();
+echo "volume bola : {$volume_bola}\n";
 
 $tabung = new Tabung();
-$tabung -> $jari_jari = (4);
-$tabung -> $tinggi =(10);
-$tabung = new Tabung (4,10);
-$volume_tabung=$tabung->volume($jari_jari,$tinggi);
-echo " volume tabung : {$volume_tabung}\n";
+$tabung -> jari_jari = 4;
+$tabung -> tinggi =10;
+$volume_tabung=$tabung->volume();
+echo "volume tabung : {$volume_tabung}\n";
 
-$nasi_tumpeng = new kerucut();
+$nasi_tumpeng = new Kerucut();
 $nasi_tumpeng->jari_jari = 4;
 $nasi_tumpeng->tinggi= 10; 
-$nasi_tumpeng = new kerucut(4,10);
-$volume_nasi_tumpeng =$nasi_tumpeng -> volume($jari_jari,$tinggi);
+$volume_nasi_tumpeng =$nasi_tumpeng -> volume();
 echo "volume kerucut : {$volume_nasi_tumpeng}";
 
 
